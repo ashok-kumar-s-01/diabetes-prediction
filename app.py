00cf273739a92c876759a2edb369a4e1996e8361
Ashok Kumar S <56039882+ashok-kumar-s-01@users.noreply.github.com>
2020-12-19 19:42:14 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[12]:
+# In[16]:
 
 
 import numpy as np
@@ -9,14 +9,14 @@ from flask import Flask,request,jsonify,render_template
 import pickle
 
 
-# In[13]:
+# In[17]:
 
 
 app = Flask(__name__)
 model = pickle.load(open(r'C:\Users\prash\Desktop\data\diabetes prediction\diabetes.pkl','rb'))
 
 
-# In[14]:
+# In[18]:
 
 
 @app.route('/')
@@ -24,7 +24,7 @@ def home():
     return render_template('index.html')
 
 
-# In[15]:
+# In[19]:
 
 
 @app.route('/predict',methods=['POST'])
@@ -34,10 +34,8 @@ def predict():
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
     
-    if prediction >0.5:
-        output = 'Diabetes'
-    else:
-        output = 'Non Diabetes'
+    output = round(prediction[0], 2)
+
         
     return render_template('index.html',prediction_text = 'You have {}'.format(output))
 
