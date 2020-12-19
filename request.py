ec581bb217a3ee7f52fcc2f7b8737d52232c59e6
Ashok Kumar S <56039882+ashok-kumar-s-01@users.noreply.github.com>
2020-12-19 19:26:58 +0530

    Add files via upload

--- request.py
+++ request.py
@@ -1,6 +1,6 @@
 import requests
 
 url = 'http://localhost:5000/predict_api'
-r = requests.post(url,json={'experience':2, 'test_score':9, 'interview_score':6})
+r = requests.post(url,json={'Pregnancies':6,'Glucose':148,'BloodPressure':72,'SkinThickness':35,'Insulin':0,'BMI':33.6,'DiabetesPedigreeFunction':0.627,'Age':50})
 
 print(r.json())
